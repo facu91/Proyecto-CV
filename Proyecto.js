fe4043a07734cdb2a201557429c9a38862b8e4eb
facu91 <126638412+facu91@users.noreply.github.com>
2023-03-01 08:49:21 -0300

    Add files via upload

---
+++ Proyecto.js
@@ -0,0 +1,14 @@
+document.getElementById("boton").onclick = function () {
+    console.log("capturamos el evento cick");
+    document.getElementById("demo").innerHTML = "Completé las órdenes de producción dentro de los plazos estrictos y dirigir a otros trabajadores del álmacen y colaboré con los líderes de equipo para mejorar la productividad."
+};
+document.getElementById("boton_Ocultar").onclick = function () {
+    document.getElementById("demo").style.display = "none";}
+
+    document.getElementById("boton1").onclick = function () {
+        console.log("capturamos el evento cick");
+        document.getElementById("dimo").innerHTML ="Seguí las instrucciones y procedimientos de seguridad durante los montajas y supervisaba la línea de montaje e informaba de cualquier problema."
+    };
+    document.getElementById("boton_Ocultar1").onclick = function () {
+        document.getElementById("dimo").style.display = "none";
+    }
